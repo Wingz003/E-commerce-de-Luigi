dc93b7e40e7b1bb64b6ddc4eff655026577811b7
Wingz003 <Lsanti618@gmail.com>
2022-01-02 21:09:52 -0600

    set up all routes and fixed associations on index.js models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,17 +6,17 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, { foreignKey: 'category_id' });
-Category.hasOne(Product, { foreignKey: 'category_id' });
+
 
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id' });
-Product.belongsTo(Category, { foreignKey: 'category_id' });
+
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: 'ProductTag', foreignKey: 'tag_id' });
+Product.belongsToMany(Tag, { through: 'product_tag', foreignKey: 'product_id' });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: 'ProductTag', foreignKey: 'product_id' });
+Tag.belongsToMany(Product, { through: 'product_tag', foreignKey: 'tag_id' });
   
 
 
